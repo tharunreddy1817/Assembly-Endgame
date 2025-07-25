f93f5dd74bb340bd54e3ed19a87895687df4d18b
Tharun Reddy Bareddy <159423568+tharunreddy1817@users.noreply.github.com>
2025-07-25 06:38:55 +0000

    Add navigation bar to funfacts

--- funFacts/src/index.tsx
+++ funFacts/src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import FunFacts from "/project/workspace/src/apps/funfacts";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <FunFacts />
   </React.StrictMode>
 );
