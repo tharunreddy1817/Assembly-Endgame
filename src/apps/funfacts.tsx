9a2d146fe9dd7021c52aeb47aa0d281a71d7ba28
Tharun Reddy Bareddy <159423568+tharunreddy1817@users.noreply.github.com>
2025-07-26 02:43:49 +0000

    Complete ReactFacts static page

--- src/apps/funfacts.tsx
+++ src/apps/funfacts.tsx
@@ -4,13 +4,9 @@ import "/project/workspace/src/styles.css";
 function Header() {
   return (
     <header className="header">
-      <img src="reactnbg.png" width="60px" alt="React Logo" />
       <nav>
-        <ul className="nav">
-          <li className="nav-item">Pricing</li>
-          <li className="nav-item">About</li>
-          <li className="nav-item">Contact</li>
-        </ul>
+        <img className="logo" src="images/reactnbg.png" alt="React Logo" />
+        <span className="logo"> ReactFacts</span>
       </nav>
     </header>
   );
