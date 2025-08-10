2e3ad2ec34111305f6953b7bda2c7aac298afe59
Tharun Reddy Bareddy <tharunreddy1817@gmail.com>
2025-08-09 23:56:13 -0700

    Overnight push

--- src/apps/endgame.tsx
+++ src/apps/endgame.tsx
@@ -1,6 +1,7 @@
 import "/home/tharunreddy1817/practice-react/src/styles.css"
 import React from "react"
 import { languages } from "../languages"
+import clsx from "clsx"
 function LanguageChip(props){
 
    return(
@@ -21,13 +22,32 @@ function Header(){
 
 
 function Main(){
-    
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
     const [currentWord, setCurrentWord] = React.useState("react")
     const chips = languages.map((data)=> <LanguageChip name={data.name} color={data.color} backgroundColor={data.backgroundColor} />)
+ 
+    const [guessedLetters, setGuessedLetters] = React.useState([])
 
+    function addGuessedLetter(letter){
+        setGuessedLetters((prev)=> prev.includes(letter) ? prev : [...prev, letter])
+    }
+    
     const wordArr = Array.from(currentWord)
-     const spans = wordArr.map((char,index)=> <span key={index} className="char">{char.toUpperCase()}</span>)
-    return (
+     const spans = wordArr.map((char,index)=>  <span key={index} className="char">{guessedLetters.includes(char) ? char.toUpperCase() : ""}</span>)
+    
+     const alphabetArr = Array.from(alphabet)
+     const keyboard = alphabetArr.map((letter,index)=> {
+     const isGuessed = guessedLetters.includes(letter)
+     const isCorrect = isGuessed && currentWord.includes(letter)
+     const isWrong = isGuessed && !currentWord.includes(letter)
+     return(
+     <button className={clsx({
+       correct : isCorrect,
+       wrong : isWrong
+     })} onClick={()=> addGuessedLetter(letter)} key={index} >{letter.toUpperCase()}</button>
+     )})
+     const wrongGuessCount = guessedLetters.filter(letter=> !currentWord.includes(letter)).length
+     return (
         <main>
             <div className="stateSection">
                 <h2>You win!</h2>
@@ -39,6 +59,11 @@ function Main(){
            <section className="char-section">
            {spans}
            </section>
+           <section className="keyboard">
+            {keyboard}
+           </section>
+           <button  className="newGame">New Game</button>
+           {wrongGuessCount}
         </main>
     )
 }
